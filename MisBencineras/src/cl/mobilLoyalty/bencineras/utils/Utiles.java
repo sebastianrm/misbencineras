fdc07ddd99d6382d0c45963c6cab8f782f8a46b4
sebastianrm <retamal.sebastian@gmail.com>
2012-08-13 18:03:51 -0400

    DESARROLLO

---
+++ MisBencineras/src/cl/mobilLoyalty/bencineras/utils/Utiles.java
@@ -0,0 +1,8 @@
+package cl.mobilLoyalty.bencineras.utils;
+
+public class Utiles {
+	
+	public static String END_POINT_FIDELIZACION="http://10.130.30.44:8080/fidelizacion/acceso/";
+	public static String END_POINT_BENCINAS_CERRCANAS="http://10.130.30.44:8080/MisBencinerasServer/cercana/";
+
+}
