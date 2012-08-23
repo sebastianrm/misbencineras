4a82df76e8465fa5d7ea7860265d9f9cf84f69ba
Sebastian <Sebastian@190.208.225.35>
2012-08-22 23:01:54 -0400

    desarrollo

--- MisBencineras/src/cl/mobilLoyalty/bencineras/utils/Utiles.java
+++ MisBencineras/src/cl/mobilLoyalty/bencineras/utils/Utiles.java
@@ -2,9 +2,8 @@ package cl.mobilLoyalty.bencineras.utils;
 
 public class Utiles {
 	
-	private static String IP_SERVER="mobilloyalty.cl";
+	private static String IP_SERVER="bencineras.ws.mobilloyalty.cl/mobilloyalty";
 //	private static String PORT_SERVER="8080";
-	
 	public static String END_POINT_FIDELIZACION="http://"+IP_SERVER+"/fidelizacion/acceso/";
 	public static String END_POINT_BENCINAS_CERRCANAS="http://"+IP_SERVER+"/MisBencinerasServer/cercana/";
 //	public static String END_POINT_FIDELIZACION="http://"+IP_SERVER+":"+PORT_SERVER+"/fidelizacion/acceso/";
